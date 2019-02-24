b06136a7e4fb3ba19ff85eb9ce5126cf402ccc9a
buyubaya <hihihi_caigikia@yahoo.com>
2019-02-24 14:02:54 +0700

    'HELLO'

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,13 +1,19 @@
 <template>
-	<div class='bookItem'>
+	<div class='book-item'>
         <h3>{{ item.name }}</h3>
         <h2>{{ item.price }}</h2>
+        <img :src="`${imgUrl}${item.img}`" />
     </div>	
 </template>
 
 <script>
+import { BASE_URL } from '@/constants/ApiUrls.js';
+
 export default {
     name: 'Item',
-    props: ['item']
+    props: ['item'],
+    data: () => ({
+        imgUrl: `${BASE_URL}/public/images/`
+    })
 }
 </script>
