8f5dd77b82705d145d367888364efa942292e571
buyubaya <hihihi_caigikia@yahoo.com>
2019-03-28 23:33:04 +0700

    HELLO

---
+++ dist/precache-manifest.e71deab1ff1bc9bf1d55b6b24d8875dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/v-book/robots.txt"
+  },
+  {
+    "revision": "b54eced642a87175fb0c",
+    "url": "/v-book/js/chunk-vendors.dde5b1e9.js"
+  },
+  {
+    "revision": "c2057942f5d88cc89207",
+    "url": "/v-book/js/app.b394fd04.js"
+  },
+  {
+    "revision": "66e5a8a10c39b77ce90a",
+    "url": "/v-book/js/about.f8e9ea73.js"
+  },
+  {
+    "revision": "72f30bbfe9c7f9ffecc16ecd3511134f",
+    "url": "/v-book/index.html"
+  },
+  {
+    "revision": "c2057942f5d88cc89207",
+    "url": "/v-book/css/app.07219c4a.css"
+  }
+];
